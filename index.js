9fd50148d65f3169175865ec5fe6779e618dc8ff
byeonsangin <sang-in.byeon@metanetglobal.com>
2024-01-19 16:38:08 +0900

    index.js

---
+++ index.js
@@ -0,0 +1,23 @@
+/*
+	index.html 에서 사용되는 js
+*/
+function login(username,password, flag) {
+	//login 처리
+	if(username !== null && password !== null && flag){
+		//username 값과 password값을 flag 체크
+		console.log('로그인처리');
+	}
+	return true;
+};
+
+function logout(username) {
+	//logout 처리
+};
+
+function r1() {
+
+};
+
+function r2() {
+
+};
